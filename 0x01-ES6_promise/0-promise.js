7f7255f2c1937e9ba0b2e861768211fd6f3a35af
loubnaa1 <loubnamerzak4@gmail.com>
2024-06-01 02:02:39 +0100

    backend-javascript

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
-export default function getResponseFromAPI() {
-  return new Promise(() => {});
+function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve();
+  });
 }
+
+export default getResponseFromAPI;
